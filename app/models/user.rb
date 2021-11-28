de63ccbb56a141943512afb33fc5a21ccf3b7f56
Ryota9 <ngm9dwf@gmail.com>
2021-11-28 14:12:42 +0900

    フォロー機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,7 +26,12 @@ class User < ApplicationRecord
   has_one :profile, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :favorite_articles, through: :likes, source: :article
+
+  has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followings, through: :following_relationships, source: :following
   
+  has_many :follower_relationships, foreign_key: 'following_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followers, through: :follower_relationships, source: :follower
   
   delegate :birthday, :age, :gender, to: :profile, allow_nil: true
 
@@ -56,4 +61,31 @@ class User < ApplicationRecord
   end
 
 
+  def follow!(user)
+    user_id = get_user_id(user)
+    following_relationships.create!(following_id: user_id)
+  end
+
+  def unfollow!(user)
+    user_id = get_user_id(user)
+    relation = following_relationships.find_by!(following_id: user_id)
+    relation.destroy!
+  end
+
+  def has_followed?(user)
+    following_relationships.exists?(following_id: user.id)
+  end
+
+
+  private
+  def get_user_id(user)
+    if user.is_a?(User)
+      user.id
+    else
+      user
+    end
+  end
+
+  
 end
+
